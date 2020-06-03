5b95194c20c33d58154146d7fb9bcf4580185bc4
Zane Dayton <day16006@byui.edu>
2020-06-03 12:12:43 -0600

    Initial commit

---
+++ app/src/main/java/byui/dayton/junkproject/MainActivity.java
@@ -0,0 +1,14 @@
+package byui.dayton.junkproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
